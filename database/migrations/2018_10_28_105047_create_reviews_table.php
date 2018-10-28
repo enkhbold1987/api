d4714fcebb8a3dd683326710697d77847ec74dac
vagrant <vagrant@vagrant.vm>
2018-10-28 11:17:54 +0000

    Migration created for product and review

--- database/migrations/2018_10_28_105047_create_reviews_table.php
+++ database/migrations/2018_10_28_105047_create_reviews_table.php
@@ -4,17 +4,21 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReviewsTable extends Migration
-{
+class CreateReviewsTable extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
@@ -24,8 +28,8 @@ class CreateReviewsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('reviews');
     }
+
 }
